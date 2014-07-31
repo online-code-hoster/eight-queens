907d296fc7118c41acd5f781272718c67f9a63cc
Vignesh Sankaran <vignesh.sankaran@me.com>
2014-07-31 10:54:43 +1000

    Building v0.2 of program

--- Setup_Array.c
+++
@@ -1,6 +0,0 @@
-/*
-	System modules
-*/
-#include <stdio.h>
-#include <stdbool.h> // For booleans in C
-#include <stdlib.h>	 // For dynamic memory management i.e. dynamic arrays
--- Setup_Array.h
+++
@@ -1,4 +0,0 @@
-#ifndef SETUP_ARRAY
-#define SETUP_ARRAY
-
-#endif
